2f9b6f3655325ffd5221fa2acef1a25213b03b14
Muhammaqdiso <iso@gmail.com>
2025-04-05 15:35:18 +0500

    TMD__0113

---
+++ js/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "blok previe card 4",
+    github: "https://github.com/TMD-0113/5-dars-4",
+    vercel:"https://5-dars-4.vercel.app"
+  },
+  {
+    title: "blok previe card 3",
+    github: "https://github.com/TMD-0113/5-dar-3",
+    vercel:"https://5-dar-3.vercel.app"
+  },
+  {
+    title: "blok previe card 2",
+    github: "https://github.com/TMD-0113/5-dars-2",
+    vercel:"https://5-dars-2-three.vercel.app"
+  },
+  {
+    title: "blok previe card",
+    github: "https://github.com/TMD-0113/5-dars-1",
+    vercel:"https://5-dars-1.vercel.app"
+  },
+  {
+    title: "Simple Omelette Recipe",
+    github: "https://github.com/TMD-0113/hatolik-bor-qiynalyapma-ozro",
+    vercel:"https://hatolik-bor-qiynalyapma-ozro.vercel.app"
+  },
+  {
+    title: "powrer",
+    github: "https://github.com/TMD-0113/power",
+    vercel:"https://power-mu.vercel.app"
+  },
+  {
+    title: "Ansormed",
+    github: "https://github.com/TMD-0113/ansormed",
+    vercel: "https://ansormed-liart.vercel.app",
+  },
+  {
+    title: "Qiziqish da yasagan saytim",
+    github: "https://github.com/TMD-0113/10-dars",
+    vercel: "https://10-dars-lovat-six.vercel.app",
+  },
+];
