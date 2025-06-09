e9ae821849ef7fdc1f5dc998dfe7dca838b3bc3d
Muhammaqdiso <iso@gmail.com>
2025-06-09 13:24:56 +0500

    ..

--- js/data.js
+++ js/data.js
@@ -1,4 +1,10 @@
 export default [
+
+  {
+    title: "music player 2",
+    github: "https://github.com/TMD-0113/4-dars",
+    vercel: "https://4-dars-eight-eta.vercel.app",
+  },
   {
     title: "music player",
     github: "https://github.com/TMD-0113/4-dars",
