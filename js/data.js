90fca10e265645251522d72d7a9ebb6434b836cd
Muhammaqdiso <iso@gmail.com>
2025-06-25 13:38:35 +0500

    .

--- js/data.js
+++ js/data.js
@@ -5,37 +5,14 @@ export default [
     vercel: "https://1-dars-eosin.vercel.app",
   },
   {
-    title: "Desserts Menu",
-    github: "https://github.com/TMD-0113/imtixon-5-oy-2-vazifa",
-    vercel: "https://imtixon-5-oy-2-vazifa.vercel.app",
-  },
-  {
-    title: "uzum sotuv bolimi",
+    title: "uzum clone",
     github: "https://github.com/TMD-0113/11-dars-uy-ishi-",
     vercel: "https://11-dars-uy-ishi-black.vercel.app",
   },
   {
-    title: "sotuv bolimi ",
-    github: "https://github.com/TMD-0113/11-darss",
-    vercel: "https://11-darss-seven.vercel.app",
-  },
-
-  {
-    title: "uzum",
-    github: "https://github.com/TMD-0113/uzum",
-    vercel: "https://uzum-beta.vercel.app",
-  },
-
-  {
     title: "sayohat",
     github: "https://github.com/TMD-0113/8-dars",
     vercel: "https://8-dars-iota.vercel.app",
-  },
-
-  {
-    title: "music player 2",
-    github: "https://github.com/TMD-0113/4-dars",
-    vercel: "https://4-dars-eight-eta.vercel.app",
   },
   {
     title: "music player",
@@ -43,52 +20,42 @@ export default [
     vercel: "https://4-dars-eight-eta.vercel.app",
   },
   {
-    title: "templet",
+    title: "Namanganliklar",
     github: "https://github.com/TMD-0113/temolettttt",
     vercel: "https://temolettttt.vercel.app",
   },
   {
-    title: "tech-book-page",
-    github: "https://github.com/TMD-0113/tech-book-landing-page",
+    title: "Tech Book club",
+    github: "https://github.com/TMD-0113/tech-book-landing-",
     vercel: "https://tech-book-landing-page-gilt.vercel.app",
   },
   {
-    title: "singe-page-desgin-porfolio",
+    title: "Simple page portfolio",
     github: "https://github.com/TMD-0113/template-scss",
     vercel: "https://template-scss-mocha.vercel.app",
   },
   {
-    title: " My Team | Home 2 qismi",
+    title: "My Team",
     github: "https://github.com/TMD-0113/3-oy-7-dars",
     vercel: "https://3-oy-7-dars-mu.vercel.app",
   },
   {
-    title: " My Team | Home",
-    github: "https://github.com/TMD-0113/3-oy-7-dars",
-    vercel: "https://3-oy-7-dars-mu.vercel.app",
-  },
-  {
-    title: "Gabrielle-Essence-Eau-De-Parfum",
+    title: "Parfume",
     github: "https://github.com/TMD-0113/Gabrielle-Essence-Eau-De-Parfum",
-    vercel: "https://gabrielle-essence-eau-de-parfum.vercel.app/",
+    vercel: "https://gabrielle-essence-eau-de-parfum.vercel.app",
   },
   {
-    title: "blok previe card 4",
+    title: "Three Column Preview Card",
     github: "https://github.com/TMD-0113/5-dars-4",
     vercel: "https://5-dars-4.vercel.app",
   },
   {
-    title: "blok previe card 3",
-    github: "https://github.com/TMD-0113/5-dar-3",
-    vercel: "https://5-dar-3.vercel.app",
-  },
-  {
-    title: "blok previe card 2",
+    title: "Social Links",
     github: "https://github.com/TMD-0113/5-dars-2",
     vercel: "https://5-dars-2-three.vercel.app",
   },
   {
-    title: "blok previe card",
+    title: "Blog Preview Card",
     github: "https://github.com/TMD-0113/5-dars-1",
     vercel: "https://5-dars-1.vercel.app",
   },
@@ -98,18 +65,29 @@ export default [
     vercel: "https://hatolik-bor-qiynalyapma-ozro.vercel.app",
   },
   {
-    title: "powrer",
+    title: "Stats Preview Card Component",
+    github: "https://github.com/TMD-0113/5-dar-3",
+    vercel: "https://5-dar-3.vercel.app",
+  },
+  {
+    title: "power",
     github: "https://github.com/TMD-0113/power",
     vercel: "https://power-mu.vercel.app",
   },
   {
-    title: "Ansormed",
-    github: "https://github.com/TMD-0113/ansormed",
-    vercel: "https://ansormed-liart.vercel.app",
+    title: "akadem nashr",
+    github: "https://github.com/TMD-0113/tayorlagan-saytim",
+    vercel: "https://tayorlagan-saytim.vercel.app",
   },
   {
-    title: "Qiziqish da yasagan saytim",
+    title: "ansormed",
     github: "https://github.com/TMD-0113/10-dars",
     vercel: "https://10-dars-lovat-six.vercel.app",
   },
+  {
+    title: "ozim yasagan 1 saytim",
+    github: "https://github.com/TMD-0113/iso-10-dars-html",
+    vercel: "https://iso-10-dars-html.vercel.app",
+  },
+  
 ];
